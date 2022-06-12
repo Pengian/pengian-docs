f9887bb05346846adf6a9e1a8b19d11f6266e29a
Pengian <pengian@foxmail.com>
2022-06-12 21:12:30 +0800

    build(deploy): first deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Pengian/pengian-docs.git main:gh-pages
+git push -f origin main:gh-pages
 
 cd -
--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,4 +4,5 @@ export default defineConfig({
   title: 'Pengian Docs',
   description: "Pengian'docs for backend",
   base: '/pengian-docs',
+  lang: 'zh',
 });
